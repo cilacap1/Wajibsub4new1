bb94e8ef48e1bd98f4b4818bb5fe4ea789a4e743
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-11-22 01:45:53 +0700

    Initialize Koyeb

---
+++ plugins/cbb.py
@@ -0,0 +1,15 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
